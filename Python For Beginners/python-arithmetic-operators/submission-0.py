8928577501308376137def54a87dd70d0f379c5b
cmconstantino-cs <cmconstantino.cs@gmail.com>
2026-07-08 20:34:30 +0800

    Add: python-arithmetic-operators - submission-0

---
+++ Python For Beginners/python-arithmetic-operators/submission-0.py
@@ -0,0 +1,11 @@
+a, b, c = 2, 2, 0.5
+
+sum_all = a + b + c
+zero_all = 0 - sum_all
+product_all = a * b * c
+div_all = sum_all / product_all
+
+print(sum_all)
+print(zero_all)
+print(product_all)
+print(div_all)
