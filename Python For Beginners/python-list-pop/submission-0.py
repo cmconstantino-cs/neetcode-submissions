dba283d0fbf2e315c23bfcc014f5dbb75164859c
cmconstantino-cs <cmconstantino.cs@gmail.com>
2026-07-15 00:45:09 +0800

    Add: python-list-pop - submission-0

---
+++ Python For Beginners/python-list-pop/submission-0.py
@@ -0,0 +1,24 @@
+from typing import List # this is used to add type hints for List type
+
+def remove_from_list(my_list: List[int], index: int) -> List[int]:
+    my_list.pop(index)
+    return my_list
+
+def pop_n_from_list(my_list: List[int], n: int) -> List[int]:
+    result = my_list
+    if n != 0:
+        while n > 0:
+            my_list.pop()
+            n -= 1
+    else:
+        pass
+    return result
+
+# don't modify below this line
+print(remove_from_list([1, 2, 3, 4, 5], 2))
+print(remove_from_list([1, 2, 3, 4, 5], 0))
+print(remove_from_list([1, 2, 3, 4, 5], 4))
+
+print(pop_n_from_list([1, 2, 3, 4, 5], 2))
+print(pop_n_from_list([1, 2, 3, 4, 5], 0))
+print(pop_n_from_list([1, 2, 3, 4, 5], 5))
