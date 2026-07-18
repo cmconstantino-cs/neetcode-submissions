aef847d2279b7b72dbf5d6c728f76f30f2c18c8a
cmconstantino-cs <cmconstantino.cs@gmail.com>
2026-07-19 00:21:30 +0800

    Add: python-dict-remove - submission-1

---
+++ Python For Beginners/python-dict-remove/submission-1.py
@@ -0,0 +1,17 @@
+from typing import Dict, List
+
+def remove_keys(my_dict: Dict[str, int], keys: List[str]) -> Dict[str, int]:
+    mod_dict = {}
+    for element in keys:
+        if element not in my_dict:
+            return my_dict
+        del my_dict[element]
+
+    return my_dict
+
+
+
+
+# do not modify below this line
+print(remove_keys({"a": 1, "b": 2, "c": 3}, ["a", "c"]))
+print(remove_keys({"a": 1, "b": 2, "c": 3}, ["d", "a"]))
